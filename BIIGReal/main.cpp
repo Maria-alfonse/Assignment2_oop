2bb92fd78ce04cc42a63b7323fdc4717c924e66b
Ahmed Abdelaziz <113646264+ahmed3ab3az@users.noreply.github.com>
2023-11-05 16:32:32 +0200

    Add files via upload

---
+++ BIIGReal/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include "BigReal.h"
+using namespace std;
+
+int main() {
+
+    // Test positive addition without +
+    BigReal n1("123.1");
+    BigReal n2("789.012");
+    BigReal sum1 = n1 + n2;
+    BigReal sum1_custom = n1+ n2;
+    cout << "n1 + n2 = " << sum1 << endl; // Expected: "912.468"
+    cout << "n1.plus(n2) = " << sum1_custom << endl; // Expected: "912.468"
+
+    // Test positive subtraction without -
+    BigReal diff1 = n2 - n1;
+    BigReal diff1_custom = n2 - n1;
+    cout << "n2 - n1 = " << diff1 << endl; // Expected: "665.556"
+    cout << "n2.minus(n1) = " << diff1_custom << endl; // Expected: "665.556"
+
+    // Test negative addition without +
+    BigReal n3("-12.4");
+    BigReal n4("-56.78");
+    BigReal sum2 = n3 + n4;
+    BigReal sum2_custom = n3 + n4;
+    cout << "n3 + n4 = " << sum2 << endl; // Expected: "-69.12"
+
+
+    // Test negative subtraction without -
+    BigReal diff2 = n3 - n4;
+    BigReal diff2_custom = n3 - n4;
+    cout << "n3 - n4 = " << diff2 << endl; // Expected: "44.44"
+
+
+    return 0;
+}
