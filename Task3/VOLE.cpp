65d5df5eea8ff0a4921761242d21f7752f586b9a
Maria-alfonse <125464100+Maria-alfonse@users.noreply.github.com>
2023-11-12 01:15:21 +0200

    Add files via upload

---
+++ Task3/VOLE.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LENOVO on 11/12/2023.
+//
+
+#include "VOLE.h"
---
+++ Task3/VOLE.h
@@ -0,0 +1,14 @@
+//
+// Created by LENOVO on 11/12/2023.
+//
+
+#ifndef TASK3_VOLE_H
+#define TASK3_VOLE_H
+
+
+class VOLE {
+
+};
+
+
+#endif //TASK3_VOLE_H
